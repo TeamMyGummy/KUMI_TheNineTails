61ad97578845a5427c3d95b304435f4ee8164267
강승연 <syeon.dev@gmail.com>
2025-07-01 15:39:41 +0900

    Feat: 경량 GAS 시스템 구현
    Feat: ASC 시스템 개선
    
    * TagContainer, GameplayAttribute public으로 열어둠
    * GA에 ASC 추가(ScriptableObject 삭제) -> 추후 외부에서 주입받을 수도 있음...

---
+++ Assets/Scripts/AbilitySystem/Base/GameplayEffect.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace AbilitySystem.Base
+{
+    public abstract class GameplayEffect
+    {
+        public string AttributeName;
+        public float Delta;
+        public ModOperation Mod = ModOperation.Additive;
+
+        public abstract void Apply(GameplayAttribute attribute);
+    }
+
+    public class InstantGameplayEffect : GameplayEffect
+    {
+        public override void Apply(GameplayAttribute attribute)
+        {
+            if (!attribute.Attributes.TryGetValue(AttributeName, out var att)) return;
+            att.Modify(Delta, Mod);
+        }
+    }
+
+    public class DurationGameplayEffect : GameplayEffect
+    {
+        public float Duration;
+        public float Interval;
+        
+        public override async void Apply(GameplayAttribute attribute)
+        {
+            if (!attribute.Attributes.TryGetValue(AttributeName, out var att)) return;
+            await ApplyWithInterval(att);
+        }
+
+        private async UniTask ApplyWithInterval(Attribute attribute)
+        {
+            int repeat = (int)(Duration / Interval);
+
+            for (int i = 0; i < repeat; i++)
+            {
+                attribute.Modify(Delta, Mod);
+                await UniTask.Delay(TimeSpan.FromSeconds(Interval), DelayType.DeltaTime);
+            }
+        }
+    }
+}
