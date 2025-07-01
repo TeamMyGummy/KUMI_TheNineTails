64cc30a1c23af6d454baa28058d7da96a3d179ea
wwonnn <dnjsgml0508@naver.com>
2025-07-02 02:22:43 +0900

    Feat: Player 인풋 바인딩
    - Input System을 이용해서 Input Binding
    - 기본적인 Move, Jump 구현

---
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private Rigidbody2D rigid;
+
+    [Range(0.0f, 3.0f)] public float speed;
+    [Range(0.0f, 7.0f)] public float jumpPower;
+    [Range(0.0f, 3.0f)] public float gravity;
+
+    private Vector2 nextDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        rigid.gravityScale = gravity;
+    }
+
+    private void FixedUpdate()
+    {
+        if(nextDirection != Vector2.zero)
+        {
+            Vector2 currMove = rigid.position;
+            Vector2 nextMove = nextDirection * speed;
+
+            rigid.position = currMove + nextMove;
+            //rigid.velocity = nextMove / Time.deltaTime;
+        }
+        
+        if(rigid.velocity.y < 0.0f)
+        {
+            //Debug.DrawRay(rigid.position, Vector2.down, new Color(1, 0, 0));
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector2.down, 1, LayerMask.GetMask("Platform"));
+            if(rayHit.collider != null)
+            {
+                if(rayHit.distance < 1.0f)
+                {
+                    GetComponent<PlayerController>().OnEnableJump();
+                }
+            }
+        }
+    }
+
+    public void Move(Vector2 direction)
+    {
+        nextDirection = direction;
+    }
+
+    public void Jump()
+    {
+        rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+    }
+}
---
+++ Assets/Scripts/InputSystem/PlayerController.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    private CharacterMovement characterMovement;
+
+    void Start()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        characterMovement = GetComponent<CharacterMovement>();
+    }
+
+    /*
+     * On_ : Input Key가 눌렸을 때 실행되는 함수
+     * OnEnable_ : Input 활성화
+     * OnDisable_ : Input 비활성화
+     */
+
+    // --------------------------- Move ---------------------------
+    public void OnMove(InputAction.CallbackContext ctx)
+    {
+        Vector2 inputDirection = ctx.ReadValue<Vector2>();
+        characterMovement.Move(inputDirection);
+    }
+
+    public void OnEnableMove()
+    {
+        playerInput.actions["Move"].Enable();
+    }
+
+    public void OnDisableMove()
+    {
+        playerInput.actions["Move"].Disable();
+    }
+
+    // --------------------------- Jump ---------------------------
+    public void OnJump()
+    {
+        characterMovement.Jump();
+        OnDisableJump();
+    }
+
+    public void OnEnableJump()
+    {
+        playerInput.actions["Jump"].Enable();
+    }
+
+    public void OnDisableJump()
+    {
+        playerInput.actions["Jump"].Disable();
+    }
+
+    // --------------------------- Dash ---------------------------
+    public void OnDash(InputAction.CallbackContext ctx)
+    {
+
+    }
+
+    public void OnEnableDash()
+    {
+        playerInput.actions["Dash"].Enable();
+    }
+
+    public void OnDisableDash()
+    {
+        playerInput.actions["Dash"].Disable();
+    }
+
+    // --------------------------- Attack ---------------------------
+    public void OnAttack(InputAction.CallbackContext ctx)
+    {
+        // tryActivate(Attack());
+    }
+
+    public void OnEnableAttack()
+    {
+        playerInput.actions["Attack"].Enable();
+    }
+
+    public void OnDisableAttack()
+    {
+        playerInput.actions["Attack"].Disable();
+    }
+
+    // --------------------------- Parrying ---------------------------
+    public void OnParrying(InputAction.CallbackContext ctx)
+    {
+        //tryActivate(Parrying());
+    }
+
+    public void OnEnableParrying()
+    {
+        playerInput.actions["Parrying"].Enable();
+    }
+
+    public void OnDisableParrying()
+    {
+        playerInput.actions["Parrying"].Disable();
+    }
+
+    // ----------------------------------------------------------------
+    public void OnEnableAllInput()
+    {
+        playerInput.ActivateInput();
+    }
+    public void OnDisableAllInput()
+    {
+        playerInput.DeactivateInput();
+    }
+}
