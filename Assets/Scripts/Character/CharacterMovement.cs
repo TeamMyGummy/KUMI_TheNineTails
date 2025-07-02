350bc5870cfa103c8dec6bde76bd2c386ab66262
wwonnn <108168611+wwonnn@users.noreply.github.com>
2025-07-02 13:07:13 +0900

    Feat: Player 인풋 바인딩
    * Chore: Input System 패키지 추가
    
    * Feat: Player 인풋 바인딩
    
    - Input System을 이용해서 Input Binding
    - 기본적인 Move, Jump 구현
    
    * Feat: 코드 수정, 태그 추가

---
+++ Assets/Scripts/Character/CharacterMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private Rigidbody2D rigid;
+
+    [SerializeField]
+    [Range(0.0f, 3.0f)] private float speed;
+
+    [SerializeField]
+    [Range(0.0f, 7.0f)] private float jumpPower;
+
+    [SerializeField]
+    [Range(0.0f, 3.0f)] private float gravity;
+
+    private Vector2 nextDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        rigid.gravityScale = gravity;
+    }
+
+    private void FixedUpdate()
+    {
+        //  Move
+        if(nextDirection != Vector2.zero)
+        {
+            Vector2 currMove = rigid.position;
+            Vector2 nextMove = nextDirection * speed;
+
+            rigid.position = currMove + nextMove;
+            //rigid.velocity = nextMove / Time.deltaTime;
+        }
+        
+        // Jump
+        if(rigid.velocity.y < 0.0f)
+        {
+            //Debug.DrawRay(rigid.position, Vector2.down, new Color(1, 0, 0));
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector2.down, 2, LayerMask.GetMask("Platform"));
+            if(rayHit.collider != null)
+            {
+                if(rayHit.distance < 0.5f)
+                {
+                    if (GetComponent<PlayerController>() != null)
+                    {
+                        GetComponent<PlayerController>().OnEnableJump();
+                    }
+                    
+                }
+            }
+        }
+    }
+
+    public void Move(Vector2 direction)
+    {
+        nextDirection = direction;
+    }
+
+    public void Jump()
+    {
+        rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+    }
+}
