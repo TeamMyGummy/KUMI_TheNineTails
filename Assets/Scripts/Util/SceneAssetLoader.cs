255e59b437e050da72560160a6202821e95c50e0
SeungYeon Kang <131231117+SYEON10@users.noreply.github.com>
2025-07-02 17:15:40 +0900

    Feat: Addressable 기반 씬 로더/에셋 로더 구현 (#5)
    * Chore: Addressable 패키지 추가
    
    * Feat: Addressable 씬 로더 구현
    
    * Feat: Addressable 기반 공용/씬 전용 에셋 로더 구현
    
    * Add: B1 씬 Addressable에 추가

---
+++ Assets/Scripts/Util/SceneAssetLoader.cs
@@ -0,0 +1,20 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace Util
+{
+    /// <summary>
+    /// Prefab, 이미지, 등의 에셋(데이터 X)을 로드하는 데 사용
+    /// 단, 씬에 종속되지 않은 에셋(여러 씬에서 사용하는 에셋 = Addressable에서 씬 단위 그룹에 포함되는 에셋)은 AssetLoader를 사용
+    /// </summary>
+    public class SceneAssetLoader : SceneSingleton<SceneAssetLoader>
+    {
+        private class InternalAssetLoader : BaseAssetLoader { }
+        private readonly InternalAssetLoader _impl = new();
+        
+        public UniTask<T> Load<T>(string key) where T : UnityEngine.Object => _impl.Load<T>(key);
+        public UniTask<GameObject> Load(string key) => _impl.Load(key);
+        public UniTask<GameObject> Spawn(string key, Transform parent = null) => _impl.Spawn(key, parent);
+        public void ReleaseAll() => _impl.ReleaseAll();
+    }
+}
