3064dd1534f490be010d29623e4b0d4f38e632e4
SeungYeon Kang <131231117+SYEON10@users.noreply.github.com>
2025-07-05 01:47:49 +0900

    Refactor: DataManager 추상 계층 추가해 등록 쉽게 변경 (#13)

--- Assets/Scripts/Managers/DataManager.cs
+++ Assets/Scripts/Managers/DataManager.cs
@@ -8,20 +8,33 @@ using Util;
 
 namespace Managers
 {
+    /// <summary>
+    /// 250705 기준) 저장 프로세스는 PR #10 참고
+    /// </summary>
     public class DataManager : Singleton<DataManager>
     {
         public PlayerState Player;
         
         public void Save(string key)
         {
-            JsonLoader.WriteDynamicData("Player", Player);
+            SaveData("Player", Player);
         }
 
         public void Load(string key)
         {
-            var data = JsonLoader.ReadDynamicData<PlayerState>("Player");
-            if (data is null) data = JsonLoader.ReadStaticData<PlayerState>("Player");
-            Player = data;
+            LoadData("Player", out Player);
+        }
+
+        private void SaveData<T>(string key, T data)
+        {
+            JsonLoader.WriteDynamicData(key, data);
+        }
+
+        private void LoadData<T>(string key, out T container)
+        {
+            var data = JsonLoader.ReadDynamicData<T>(key);
+            if (data is null) data = JsonLoader.ReadStaticData<T>(key);
+            container = data;
         }
         
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
