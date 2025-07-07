c4d5b1d6ad27d880dd289c6851049b9385ec5755
Gammja17 <haker_40@naver.com>
2025-07-07 18:29:28 +0900

    Fix: GAS 시스템 적용
    Fix: hp바 코드 수정, 불필요한 스크립트 삭제
    
    Feat: 몬스터 Attribute 추가
    
    잡몹 회의록 기반 (수정 가능)

--- Assets/Scripts/Monster/UI_MonsterHpBar.cs
+++ Assets/Scripts/Monster/UI_MonsterHpBar.cs
@@ -1,8 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
-using AbilitySystem.Base;
+using R3;
+using System.Collections;
 
 public class UI_MonsterHpBar : MonoBehaviour
 {
@@ -11,72 +10,68 @@ public class UI_MonsterHpBar : MonoBehaviour
 
     private RectTransform hpBar;
     private Image hpImage;
-    private Attribute hp;
-
-    private float damagedTime = 0f; //마지막으로 피격된 시각 기록
+    private GameAbilitySystem.Attribute hp;
     private CanvasGroup canvasGroup;
+    private CompositeDisposable _disposables = new();
 
-    void Start()
+    private float damagedTime;
+
+    private IEnumerator Start()
     {
+        // ASC가 초기화될 때까지 기다림
+        var monster = GetComponent<Monster>();
+        while (!monster.asc.Attribute.Attributes.ContainsKey("HP"))
+        {
+            yield return null;
+        }
 
-        // HP 바 인스턴스 생성
+        hp = monster.asc.Attribute.Attributes["HP"];
+
         hpBar = Instantiate(prfHpBar, canvas.transform).GetComponent<RectTransform>();
-        hpBar.gameObject.SetActive(false); //비활성화
-
+        hpBar.gameObject.SetActive(false);
         canvasGroup = hpBar.GetComponent<CanvasGroup>();
+        hpImage = hpBar.transform.Find("hp_bar").GetComponent<Image>();
 
-        // hp바의 이미지를 찾기 (실제로 줄어드는 이미지)
-        hpImage = hpBar.transform.Find("hp_bar").GetComponent<Image>(); 
-
-        var ga = GetComponent<GameplayAttribute>();
-        ga.Attributes.TryGetValue("HP", out var hpAttribute);
-        //hp스탯 찾아서 담기
-        hp = hpAttribute;
-        hpImage.fillAmount = hp.CurrentValue / hp.MaxValue; //hp 비율로 이미지 조정됨
-        hp.OnValueChanged += OnHpChanged;
+        hpImage.fillAmount = hp.CurrentValue.Value / hp.MaxValue;
+        _disposables.Add(hp.CurrentValue.Subscribe(OnHpChanged));
     }
 
     void Update()
     {
         if (hpBar == null) return;
 
-        // 몬스터 키 구하기
         var renderer = GetComponentInChildren<Renderer>();
         float height = renderer.bounds.max.y;
-
         Vector3 hpBarPos = Camera.main.WorldToScreenPoint(new Vector3(transform.position.x, height + 0.2f, transform.position.z));
         hpBar.position = hpBarPos;
 
         float damageTimer = Time.time - damagedTime;
-        if (damageTimer <= 0.7f) //0.7초동안 선명하게 표기
+        if (damageTimer <= 0.7f)
             canvasGroup.alpha = 1f;
-        else if (damageTimer <= 1.0f) { //0.7초 이내 재피격당하지 않을 경우 0.3초동안 페이드아웃
+        else if (damageTimer <= 1.0f)
+        {
             float t = (damageTimer - 0.7f) / 0.3f;
-            canvasGroup.alpha = Mathf.Lerp(1f, 0f, t); //페이드아웃
+            canvasGroup.alpha = Mathf.Lerp(1f, 0f, t);
         }
         else
         {
             canvasGroup.alpha = 0f;
             hpBar.gameObject.SetActive(false);
         }
-
     }
 
     void OnHpChanged(float newHp)
     {
         hpImage.fillAmount = newHp / hp.MaxValue;
-
-        // 피격 시 hp바 띄움
         if (newHp < hp.MaxValue)
         {
             damagedTime = Time.time;
             hpBar.gameObject.SetActive(true);
-  
         }
     }
 
     void OnDestroy()
     {
-        //Destroy(hpBar.gameObject);
+        _disposables.Dispose();
     }
 }
