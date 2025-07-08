7e722ba1d60233ac23b9d74a0ae3acc65242ff4b
SeungYeon Kang <syeon.dev@gmail.com>
2025-07-08 18:04:08 +0900

    Test: 데이터 저장/불러오기 테스트 씬 추가

---
+++ Assets/Scripts/Map/TestMap/TestMapScene.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using Util;
+
+public class TestMapScene : SceneSingleton<TestMapScene>
+{
+    public TestMapState State;
+    new void Awake()
+    {
+        base.Awake();
+        DomainFactory.Instance.GetState(StateKey.TestMap, out State);
+    }
+}
