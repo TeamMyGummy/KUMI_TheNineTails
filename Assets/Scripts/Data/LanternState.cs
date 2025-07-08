9b295ae83f08b8270ecca68226a493e4b2f136af
SeungYeon Kang <syeon.dev@gmail.com>
2025-07-08 18:04:53 +0900

    WIP: 호롱불 일부 구현

---
+++ Assets/Scripts/Data/LanternState.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LanternState
+{
+    public HashSet<int> PassedCheckPoint = new();
+    public int RecentCheckPoint = -1;
+    public string RecentScene = String.Empty;
+}
