2468be7b5d6c63cdbc43f3cf9fb18a497e78f2ec
nyamic <kimh@ewha.ac.kr>
2025-07-12 19:18:36 +0900

    Refactor: 장애물-GE 예제코드로 다시 수정

--- Assets/Scripts/Map/SteamChild.cs
+++ Assets/Scripts/Map/SteamChild.cs
@@ -5,34 +5,25 @@ using UnityEngine;
 public class SteamChild : MonoBehaviour
 {
     private SteamObject parentSteam;
-    private PlayerRespawnController _playerRespawnController;
-    private Damageable _playerDamageable;
 
     private void Start()
     {
         parentSteam = GetComponentInParent<SteamObject>();
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-
-        if (player != null)
-        {
-            _playerRespawnController = player.GetComponent<PlayerRespawnController>();
-            _playerDamageable = player.GetComponent<Damageable>();
-        }
-
-        if (_playerRespawnController == null || _playerDamageable == null)
-        {
-            Debug.LogError("[SpikeObject] 플레이어 관련 컴포넌트를 찾을 수 없습니다");
-        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!collision.CompareTag("Player")) return;
-        
+
         if (parentSteam != null && parentSteam.IsActive())
         {
-            _playerDamageable?.GetDamage(DomainKey.Player, parentSteam.GetDamage());
-            _playerRespawnController?.Respawn();
+            collision.GetComponent<Damageable>()?.GetDamage(DomainKey.Player, parentSteam.GetDamage());
+            
+            var controller = collision.GetComponent<PlayerRespawnController>();
+            if (controller != null)
+            {
+                controller.Respawn();
+            }
         }
     }
 }
