ea96f2fa4bda12bdaeb6f250646648985b38f943
nyamic <kimh@ewha.ac.kr>
2025-07-12 18:03:32 +0900

    Feat: 장애물에 Respawn 코드 추가
    +Spike, Laser, Steam 리팩토링(매번 GetComponent 대신 캐싱)

--- Assets/Scripts/Map/LaserObject.cs
+++ Assets/Scripts/Map/LaserObject.cs
@@ -5,12 +5,29 @@ using UnityEngine;
 public class LaserObject : MonoBehaviour
 {
     [SerializeField] private float damage = 1.0f;
+    private PlayerRespawnController _playerRespawnController;
+    private Damageable _playerDamageable;
+    private void Start()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
 
+        if (player != null)
+        {
+            _playerRespawnController = player.GetComponent<PlayerRespawnController>();
+            _playerDamageable = player.GetComponent<Damageable>();
+        }
+
+        if (_playerRespawnController == null || _playerDamageable == null)
+        {
+            Debug.LogError("[SpikeObject] 플레이어 관련 컴포넌트를 찾을 수 없습니다");
+        }
+    }
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
-        {
-            collision.gameObject.GetComponent<Damageable>().GetDamage(DomainKey.Player, damage);
-        }
+        if (!collision.CompareTag("Player")) return;
+        
+        _playerDamageable?.GetDamage(DomainKey.Player, damage);
+        _playerRespawnController?.Respawn();
     }
 }
--- Assets/Scripts/Map/SteamChild.cs
+++ Assets/Scripts/Map/SteamChild.cs
@@ -5,10 +5,24 @@ using UnityEngine;
 public class SteamChild : MonoBehaviour
 {
     private SteamObject parentSteam;
+    private PlayerRespawnController _playerRespawnController;
+    private Damageable _playerDamageable;
 
     private void Start()
     {
         parentSteam = GetComponentInParent<SteamObject>();
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        if (player != null)
+        {
+            _playerRespawnController = player.GetComponent<PlayerRespawnController>();
+            _playerDamageable = player.GetComponent<Damageable>();
+        }
+
+        if (_playerRespawnController == null || _playerDamageable == null)
+        {
+            Debug.LogError("[SpikeObject] 플레이어 관련 컴포넌트를 찾을 수 없습니다");
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -17,7 +31,8 @@ public class SteamChild : MonoBehaviour
         
         if (parentSteam != null && parentSteam.IsActive())
         {
-            collision.GetComponent<Damageable>()?.GetDamage(DomainKey.Player, parentSteam.GetDamage());
+            _playerDamageable?.GetDamage(DomainKey.Player, parentSteam.GetDamage());
+            _playerRespawnController?.Respawn();
         }
     }
 }
