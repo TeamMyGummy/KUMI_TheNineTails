6f890ed102d265bc6363895b70e00c3b52522850
Gammja17 <haker_40@naver.com>
2025-07-14 14:56:37 +0900

    Feat: 몬스터 ai 추가
    기본 이동 / 플레이어 어그로 끌리는 것 / 그 외 데이터 구조 수정

--- Assets/Scripts/Monster/Monster.cs
+++ Assets/Scripts/Monster/Monster.cs
@@ -8,7 +8,7 @@ public class Monster : MonoBehaviour
     public AbilitySystem asc { get; private set; }
 
     [SerializeField] private string monsterName = "";
-    [SerializeField] private bool isCreature = true; //생물(true) / 기계(false)
+    [SerializeField] private bool isParrying = true; //패링가능하면 true -> 필요할지 모르겠는데 일단 넣어놓음
     [SerializeField] private bool isMoving = true; //능동(true) /수동(false)
 
     private void Awake()
@@ -19,8 +19,6 @@ public class Monster : MonoBehaviour
         asc.Init("Test/TestMonster1");
         asc.GrantAllAbilities();
     }
-
-    private float damageTimer = 0f;
 
     private void Update()
     {
@@ -34,18 +32,7 @@ public class Monster : MonoBehaviour
             }
         }
 
-        //여기서부턴 hp바 잘 뜨는지 보기 위한 임시 데미지입히기코드..
-        damageTimer += Time.deltaTime;
-        if (damageTimer >= 2f)
-        {
-            damageTimer = 0f;
-
-            if (asc.Attribute.Attributes.TryGetValue("HP", out var HP))
-            {
-                hp.Modify(-10f, ModOperation.Additive);
-                Debug.Log($"[Test] {monsterName}에게 10 데미지를 입혔습니다. 남은 체력: {hp.CurrentValue.Value}");
-            }
-        }
+        
     }
 
     private void Die()
