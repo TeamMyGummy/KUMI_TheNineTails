eb477631f8a47fc4b3f8ceec638d8e2b2cc20c9d
lastgamer05 <105638752+lastgamer05@users.noreply.github.com>
2025-07-21 16:33:43 +0900

    Feat: Yarn 대사 출력 및 BaseScreen 구현 (#37)
    * Feat: yarnSpinner Setting
    
    * Feat: yarn script파일 대사 출력
    
    * Chore: 인풋 방식 Both로 바꿈
    
    * Feat: BaseScreen 구현
    
    * Rename: Screen.cs 변수명 변경
    
    * Chore: Dotween 설치
    
    * Add: 캐릭터 대화 스프라이트 리소스 폴더 생성
    
    * Add: 대화 시스템 테스트용 이미지 추가
    
    * Feat: show, hide, change 함수 구현
    
    * Feat: 페이드 인 아웃 함수 수정

---
+++ Assets/Scripts/UI/Base/Screen.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Screen : MonoBehaviour
+{
+
+    private Image _targetImage;
+
+    private void Awake()
+    {
+        _targetImage = GetComponent<Image>();
+    }
+
+    /// <summary>
+    /// 화면 활성화
+    /// </summary>
+    public void ShowScreen()
+    {
+        if(!gameObject.activeSelf)
+            gameObject.SetActive(true);
+    }
+
+    /// <summary>
+    /// 화면 비활성화
+    /// </summary>
+
+    public void HideScreen()
+    {
+        if (gameObject.activeSelf)
+            gameObject.SetActive(false);
+    }
+
+    /// <summary>
+    /// 알파값 조절
+    /// </summary>
+
+    public void FadeScreen(float duration, float startAlpha, float targetAlpha)
+    {
+        if (_targetImage != null)
+        {
+            StartCoroutine(FadeToAlpha(startAlpha, targetAlpha, duration));
+        }
+    }
+    
+
+    private IEnumerator FadeToAlpha(float startAlpha, float targetAlpha, float duration)
+    {
+        Color color = _targetImage.color;
+        float start = startAlpha;
+        float time = 0f;
+
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            float newAlpha = Mathf.Lerp(start, targetAlpha, time / duration);
+            _targetImage.color = new Color(color.r, color.g, color.b, newAlpha);
+            yield return null;
+        }
+        
+        _targetImage.color = new Color(color.r, color.g, color.b, targetAlpha);
+    }
+}
