5d5455c6e523e1d6cf89598b7a3e2349535525b4
wwonnn <dnjsgml0508@naver.com>
2025-07-21 13:19:57 +0900

    Feat: 몬스터 데미지 추가

--- Assets/Scripts/Character/Damageable.cs
+++ Assets/Scripts/Character/Damageable.cs
@@ -22,4 +22,20 @@ public class Damageable : MonoBehaviour
         
         Debug.Log($"Damage: {damage}, HP: {att.Attributes["HP"].CurrentValue}");
     }
+    
+    /// <summary>
+    /// 데미지를 입었을 때 처리 (플레이어 제외)
+    /// </summary>
+    /// <param name="asc">데미지를 입는 대상이 가지고 있는 Ability System</param>
+    /// <param name="damage">입은 데미지 양</param>
+    public void GetDamage(AbilitySystem asc, float damage)
+    {
+        // GE
+        GameplayAttribute att = asc.Attribute;
+
+        InstantGameplayEffect effect = new("HP", damage * (-1));
+        effect.Apply(att);
+        
+        Debug.Log($"Damage: {damage}, HP: {att.Attributes["HP"].CurrentValue}");
+    }
 }
