67a55b999345e7f1ff3c226123788cd71d5c419f
wwonnn <dnjsgml0508@naver.com>
2025-07-21 13:19:57 +0900

    Feat: 몬스터 데미지 추가

--- Assets/Scripts/Player/AttackEffect.cs
+++ Assets/Scripts/Player/AttackEffect.cs
@@ -1,14 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using GameAbilitySystem;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AttackEffect : MonoBehaviour
 {
+    private GameObject _actor;
+    private CharacterMovement _cm;
+    private Vector2 _spawnPoint = new Vector2(0.4f, 0.75f);
+
+    void Start()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+        /*_actor = GetComponentInParent<Player>().gameObject;
+        _cm = _actor.GetComponent<CharacterMovement>();
+        gameObject.transform.position = new Vector2(_cm.GetCharacterDirection().x * _spawnPoint.x, _spawnPoint.y);*/
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Enemies")
         {
-            collision.gameObject.GetComponent<Damageable>().GetDamage(DomainKey.Player, 2.0f);
+            AbilitySystem asc = collision.GetComponent<Monster>().asc;
+            collision.gameObject.GetComponent<Damageable>().GetDamage(asc, 2.0f);
         }
     }
 }
