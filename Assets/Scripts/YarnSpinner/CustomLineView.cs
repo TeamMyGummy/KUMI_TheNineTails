4e4649219c72ba54291081f4ace3df8e4a6642a1
daeun,Kang <kangda0519@gmail.com>
2025-07-25 02:12:19 +0900

    Feat: 대화 스페이스 바 누르면 넘어가게 구현

--- Assets/Scripts/YarnSpinner/CustomLineView.cs
+++ Assets/Scripts/YarnSpinner/CustomLineView.cs
@@ -16,7 +16,31 @@ public class CustomLineView : LineView
         // 여기에 이벤트 호출 추가
         onLineStarted?.Invoke(line);
 
+
         // 기존 기능 수행
         base.RunLine(line, onLineComplete);
+        StartCoroutine(RunLineCoroutine(line, onLineComplete));
     }
+
+    private IEnumerator RunLineCoroutine(LocalizedLine line, System.Action onLineComplete)
+    {
+        if (line.Text.Text.StartsWith("->"))
+        {
+            yield break;
+        }
+        
+        yield return StartCoroutine(WaitForSpace());
+        
+        onLineComplete?.Invoke();
+    }
+
+    private IEnumerator WaitForSpace()
+    {
+        //Debug.Log("Waiting for space...");
+        while (!Input.GetKeyDown(KeyCode.Space))
+        {
+            yield return null;
+        }
+    }
+   
 }
