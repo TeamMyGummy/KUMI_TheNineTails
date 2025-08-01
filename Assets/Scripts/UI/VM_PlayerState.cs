bdb9020012005c539219beab640a4023b5f82ccc
ohjinyxung <esujie339@ewhain.net>
2025-08-01 18:25:46 +0900

    Feat:인게임 UI 여우불 연결

--- Assets/Scripts/UI/VM_PlayerState.cs
+++ Assets/Scripts/UI/VM_PlayerState.cs
@@ -10,13 +10,24 @@ namespace UI
         private AbilitySystem _playerModel;
         public ReadOnlyReactiveProperty<float> Hp { get; private set; }
         public ReadOnlyReactiveProperty<int> SkillCount { get; private set; }
+        public ReadOnlyReactiveProperty<float> FoxFireGauge { get; private set; }
+        public ReadOnlyReactiveProperty<int> FoxFireCount { get; private set; }
 
         public void Awake()
         {
             DomainFactory.Instance.GetDomain(DomainKey.Player, out _playerModel);
 
+            if (_playerModel == null)
+            {
+                return;
+            }
+
             Hp = _playerModel.Attribute.Attributes["HP"].CurrentValue;
+            FoxFireGauge = _playerModel.Attribute.Attributes["FoxFireGauge"].CurrentValue.ToReadOnlyReactiveProperty();
+            FoxFireCount = _playerModel.Attribute.Attributes["FoxFireCount"].CurrentValue.Select(x => (int)x).ToReadOnlyReactiveProperty();
+
             SkillCount = _playerModel.GrantedAbilityCount;
         }
+
     }
 }
