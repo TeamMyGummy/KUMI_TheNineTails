155f985dd0e442fe94cd9074f2dc486c634ded8f
SeungYeon Kang <syeon.dev@gmail.com>
2025-08-01 23:48:18 +0900

    Fix: 히트박스 뎀딜 안 들어가는 문제 해결

--- Assets/Scripts/AbilitySystem/Abilities/Hitbox.cs
+++ Assets/Scripts/AbilitySystem/Abilities/Hitbox.cs
@@ -17,7 +17,8 @@ public class Hitbox : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (_boxCollider == null) return;
-        if (other.gameObject.layer != LayerMask.NameToLayer("Parrying")) return;
+        if (other.gameObject.layer != LayerMask.NameToLayer("Monster")) return;
+        //todo. 이 부분 가능하다면 그냥 설정 단의 레이어 충돌에서 제거하기
         
         Vector2 boxCenter = (Vector2)transform.position + _boxCollider.offset;
         Vector2 boxSize = _boxCollider.size;
@@ -30,7 +31,7 @@ public class Hitbox : MonoBehaviour
         if (size > 0)
         {
             Debug.Log("패링 성공");
-            //todo: 아직까지는 문제 없는데 이걸 여기다 두는 게 맞는가
+            //todo: 아직까지는 문제 없는데 이걸 여기다 두는 게 맞는가 -> 분리할 수 있다면 분리할 것
             AbilitySystem asc;
             DomainFactory.Instance.GetDomain(DomainKey.Player, out asc);
             asc.ApplyGameplayEffect(asc, new InstantGameplayEffect("FoxFireGauge", 1));
