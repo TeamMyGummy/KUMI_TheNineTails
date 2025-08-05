da34b49468deb692fbf0d870a87b467f73dfc1b0
nyamic <kimh@ewha.ac.kr>
2025-08-05 19:59:29 +0900

    Fix: 이동플랫폼-플레이어가 공중에 있을 때만 활성화

--- Assets/Scripts/Map/Platform/MovingPlatform.cs
+++ Assets/Scripts/Map/Platform/MovingPlatform.cs
@@ -10,19 +10,25 @@ public class MovingPlatform : MonoBehaviour
 
     private Transform _pointA;
     private Transform _pointB;
+    private Transform _playerOnPlatform;
     
     private Vector2 _currentTarget;
     private Vector2 _worldPointA;
     private Vector2 _worldPointB;
-    private Transform _playerOnPlatform;
-    private Rigidbody2D _rb;
     
-    private bool _isWaiting = false;
+    private Rigidbody2D _platformRb;
+    private Rigidbody2D _playerRb;
+    
     private float _waitTimer = 0f;
+	private float _correctionTime = 2f;
+    
+    private bool _isVertical;
+    public bool IsWaiting { get; private set; }
+   
 
     private void Awake()
     {
-        _rb = GetComponent<Rigidbody2D>();
+        _platformRb = GetComponent<Rigidbody2D>();
         
         _pointA = transform.GetChild(1);
         _pointB = transform.GetChild(2);
@@ -30,21 +36,29 @@ public class MovingPlatform : MonoBehaviour
 
     private void Start()
     {
+        IsWaiting =  false;
+            
         _worldPointA = _pointA.position;
         _worldPointB = _pointB.position;
         
         _currentTarget = _worldPointB;
-        /*StartCoroutine(MovePlatform());*/
+        
+        // 이동방향 판단 (상하/좌우)
+        Vector2 delta = _worldPointB - _worldPointA;
+        _isVertical = Mathf.Abs(delta.y) > Mathf.Abs(delta.x) ? true : false;
     }
 
+    /// <summary>
+    /// 플랫폼 이동
+    /// </summary>
     private void FixedUpdate()
     {
-        if (_isWaiting)
+        if (IsWaiting)
         {
             _waitTimer += Time.deltaTime;
             if (_waitTimer >= waitTime)
             {
-                _isWaiting = false;
+                IsWaiting = false;
                 _waitTimer = 0f;
                 _currentTarget = (_currentTarget == _worldPointA) ? _worldPointB : _worldPointA;
             }
@@ -56,48 +70,55 @@ public class MovingPlatform : MonoBehaviour
         Vector2 newPosition = Vector2.MoveTowards(currentPosition, _currentTarget, speed * Time.deltaTime);
         Vector2 deltaPosition = newPosition - currentPosition;
 
-        _rb.MovePosition(newPosition);
+        _platformRb.MovePosition(newPosition);
+        /*MovePlayerWithPlatform(deltaPosition);*/
 
         if (Vector2.Distance(newPosition, _currentTarget) < 0.01f)
         {
-            _isWaiting = true;
+            IsWaiting = true;
         }
     }
-
-    /*private IEnumerator MovePlatform()
-    {
-        while (true)
-        {
-            Vector2 startPosition = transform.position;
-            Vector2 targetPosition = _currentTarget;
-            
-            while (Vector2.Distance(transform.position, targetPosition) > 0.01f)
-            {
-                Vector2 newPosition = Vector2.MoveTowards((Vector2)transform.position, targetPosition, speed * Time.deltaTime);
-                Vector2 deltaPosition = newPosition - (Vector2)transform.position;
-                
-                _rb.MovePosition(newPosition);
-                MovePlayerWithPlatform(deltaPosition);
-                
-                yield return null;
-            }
-            
-            _currentTarget = (_currentTarget == _worldPointA) ? _worldPointB : _worldPointA;
-            
-            yield return new WaitForSeconds(waitTime);
-        }
-    }*/
     
     public void SetPlayer(Transform player)
     {
         _playerOnPlatform = player;
-        Debug.Log("SetPlayer");
+        
+        if (_playerRb == null) 
+            _playerRb = player.GetComponent<Rigidbody2D>();
     }
     
     public void RemovePlayer()
     {
         _playerOnPlatform = null;
-        Debug.Log("RemovePlayer");
     }
+
+    /*private void MovePlayerWithPlatform(Vector2 delta)
+    {
+        if (Mathf.Abs(_playerRb.velocity.x) < 0.01f)
+        {
+            _playerOnPlatform.position += (Vector3)delta;
+            Debug.Log("MovePlayerWithPlatform 실행");
+        }
+    }*/
+    
+    
+
+    // 끝부분 도착했을 때 플레이어 미끄러지거나 튀어오르지 않게 보정
+    // rb 캐싱이 비용이 많이 든다길래 mp에다 적었는데 굳이이럴필요없나... 서희언니 보고있나요? 알려주실래요?
+    public void ApplyPlatformVelocity()
+    {
+        Vector2 platformVelocity = _platformRb.velocity;
+        
+        if (_isVertical)
+        {
+            _playerRb.velocity = new Vector2(_playerRb.velocity.x, platformVelocity.y);
+        }
+        else
+        {
+            _playerRb.velocity = new Vector2(platformVelocity.x, _playerRb.velocity.y);
+        }
+        
+    }
+    
     
 }
