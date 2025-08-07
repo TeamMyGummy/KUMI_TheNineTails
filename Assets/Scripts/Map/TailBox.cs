322b193e8bfc157ad927352b382cece63279fda4
ohjinyxung <esujie339@ewhain.net>
2025-08-07 21:02:31 +0900

    Fix: 스킬 아이템 습득 후 스킬 안내창
    기존 TailBox 스크립트 SerializeField로 SKillInfo (스킬 안내창) 프리팹 연결, 하이라키창 UI-BaseCanvas 연결하면 됨
    
    (UI 시스템 기획서 2.3.3)

--- Assets/Scripts/Map/TailBox.cs
+++ Assets/Scripts/Map/TailBox.cs
@@ -8,16 +8,19 @@ public class TailBox : MonoBehaviour
     [SerializeField] private GameObject interactionUI;
     [SerializeField] private AbilityKey key = AbilityKey.PlayerAttack;
     [SerializeField] private AbilityName name = AbilityName.PlayerAttack;
-    
+    private SkillInfo skillInfoInstance;
+    [SerializeField] private SkillInfo skillInfoPrefab;
+    [SerializeField] private Transform baseCanvas;
+
     private AbilitySystem _playerModel;
     private bool _playerInRange = false;
     private bool _isUsed = false;
-    
+
     public void Awake()
     {
         DomainFactory.Instance.GetDomain(DomainKey.Player, out _playerModel);
     }
-    
+
     private void Start()
     {
         if (interactionUI != null)
@@ -39,17 +42,18 @@ public class TailBox : MonoBehaviour
             if (success)
             {
                 Debug.Log("스킬 부여 성공!");
+                ShowSkillInfoPopup(key, name);
             }
             else
             {
                 Debug.LogWarning("스킬 부여 실패: 해당 키/이름에 맞는 스킬이 존재하지 않음.");
             }
-            
+
             _isUsed = true;
             interactionUI.SetActive(false);
         }
     }
-    
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (_isUsed == false)
@@ -67,7 +71,7 @@ public class TailBox : MonoBehaviour
             }
         }
     }
-    
+
     private void OnTriggerExit2D(Collider2D other)
     {
         if (_isUsed == false)
@@ -78,5 +82,24 @@ public class TailBox : MonoBehaviour
                 interactionUI.SetActive(false);
             }
         }
+    }
+    
+    // 스킬 정보 팝업 띄우기
+    private void ShowSkillInfoPopup(AbilityKey key, AbilityName name)
+    {
+        var instance = Instantiate(skillInfoPrefab, baseCanvas);
+
+        // 스킬 정보 가져오기
+        var so = _playerModel.GetSkillSO(key);
+        if (so != null)
+        {
+            instance.SetSkillInfo(so);
+        }
+        else
+        {
+            Debug.LogWarning($"[TailBox] 스킬 정보가 없습니다: {key}, {name}");
+        }
+
+        instance.gameObject.SetActive(true);
     }
 }
---
+++ Assets/Scripts/UI/Popup/SkillInfo.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using GameAbilitySystem;
+using UnityEngine.InputSystem;
+
+public class SkillInfo : MonoBehaviour
+{
+    [Header("UI")]
+    [SerializeField] private TextMeshProUGUI skillNameText;
+    [SerializeField] private TextMeshProUGUI skillDescriptionText;
+    [SerializeField] private Image skillIconImage;
+    [SerializeField] private Image skillKeyImage;
+
+    public void SetSkillInfo(GameplayAbilitySO so)
+    {
+        skillNameText.text = so.skillName.ToString();
+        skillDescriptionText.text = so.description;
+        skillIconImage.sprite = so.skillIcon;
+        skillKeyImage.sprite = so.skillKeyboard;
+    }
+    
+     private void Update()
+    {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            OnClick_Close();
+        }
+    }
+
+    public void OnClick_Close()
+    {
+        Destroy(gameObject);
+    }
+}
