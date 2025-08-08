d8301aaca313d2dd0b7ca5bcc08753c46a59bb01
nyamic <kimh@ewha.ac.kr>
2025-08-08 13:13:44 +0900

    Feat: BreakableWall 파괴여부 영구저장

--- Assets/Scripts/Map/BreakableWall.cs
+++ Assets/Scripts/Map/BreakableWall.cs
@@ -1,22 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Util;
 
 public class BreakableWall : MonoBehaviour
 {
-    private int _attackCount = 0; 
+    private int _attackCount = 0;
+    private const int Maxhit = 2;
+    private int _breakableWallKey;
 
+    private void Awake()
+    {
+        _breakableWallKey = SceneLoader.GetCurrentSceneName().StringToInt() + transform.GetSiblingIndex();
+        
+        //파괴된 벽 --> 시작할 때 삭제 
+        if (DomainFactory.Instance.Data.BreakableWallState.BrokenWalls.Contains(_breakableWallKey))
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    /// <summary>
+    /// 일반공격 2번(Maxhit) --> 벽 파괴, 파괴된 벽 정보 저장 
+    /// </summary>
     public void AttackCount()
     {
         _attackCount += 1;
         
-        if(_attackCount == 2)
+        if(_attackCount == Maxhit)
             Broken();
     }
     
     private void Broken()
     {
-        /*Debug.Log("[BreakableWall] is broken");*/
+        DomainFactory.Instance.Data.BreakableWallState.BrokenWalls.Add(_breakableWallKey);
+        DomainFactory.Instance.SaveGameData();
         Destroy(this.gameObject);
     }
 }
