e9b3beb6aa4550ef022b2c5c45d24e05e4f262ec
wwonnn <dnjsgml0508@naver.com>
2025-08-11 16:53:49 +0900

    Feat: B2 보스 씬 연결

---
+++ Assets/Scripts/ButtonTest.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ButtonTest : MonoBehaviour
+{
+    public string nextScene;
+    public void ToB2BossScene()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+}
