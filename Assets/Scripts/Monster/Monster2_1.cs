f6fb2e683a30c1510529341995f03d972f599f2b
daeun,Kang <kangda0519@gmail.com>
2025-08-14 16:58:12 +0900

    Feat: Monster 2_1 구현

---
+++ Assets/Scripts/Monster/Monster2_1.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster2_1 : Monster
+{
+    public AbilityKey abilityKey;
+	private bool isAttacking = false;
+    private int cnt = 0;
+    private float attackDelay = 0.5f;
+    protected override void EnterAttackRange()
+    {
+        if (!isAttacking)
+        {
+            StartCoroutine(Block(2f));
+        }
+    }
+
+    private IEnumerator Block(float time)
+    {
+        isAttacking = true;
+        for (int i = 0; i < 3; i++)
+        {
+            asc.TryActivateAbility(abilityKey);
+
+            if (i < 2)
+            {
+                yield return new WaitForSeconds(attackDelay);
+            }
+        }
+        yield return new WaitForSeconds(time);
+        isAttacking = false;
+    }
+}
