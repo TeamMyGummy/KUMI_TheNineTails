5086b585a4ec00217c64d6ad6f9bc2f6857a6d44
wwonnn <dnjsgml0508@naver.com>
2025-08-14 21:17:34 +0900

    Feat: 밧줄 타기 구현

---
+++ Assets/Scripts/Map/Platform/Rope.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+    private List<(Transform child, Vector2 originPos)> _segments = new List<(Transform, Vector2)>();
+    
+    private void Start()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            _segments.Add((transform.GetChild(i), transform.GetChild(i).position));
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (other.gameObject.TryGetComponent<CharacterMovement>(out var cm) && other.gameObject.TryGetComponent<PlayerController>(out var pc))
+            {
+                if (!cm.CheckIsGround() && !cm.CheckIsRopeClimbing() && pc.IsPressedClimbKey())
+                {
+                    pc.StartRopeClimb();
+                    cm.StartRopeClimbState();
+                    other.transform.position = new Vector2(transform.position.x, other.transform.position.y);
+
+                    for (int i = 0; i < transform.childCount; i++)
+                    {
+                        _segments[i].child.position = _segments[i].originPos;
+                    }
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (other.gameObject.TryGetComponent<CharacterMovement>(out var cm))
+            {
+                if (cm.CheckIsRopeClimbing())
+                {
+                    cm.EndRopeClimbState();
+                    cm.Move(Vector2.zero);
+                    other.gameObject.GetComponent<PlayerController>().EndRopeClimb();
+                }
+            }
+        }
+    }
+}
