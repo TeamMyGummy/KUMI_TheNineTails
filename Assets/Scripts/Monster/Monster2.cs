4311af7a70149b82573c3221ec3de85a2ce7d75c
daeun,Kang <kangda0519@gmail.com>
2025-08-14 16:59:02 +0900

    Fix: Monster2 공격방법 수정

--- Assets/Scripts/Monster/Monster2.cs
+++ Assets/Scripts/Monster/Monster2.cs
@@ -5,8 +5,20 @@ using UnityEngine;
 public class Monster2 : Monster
 {
     public AbilityKey abilityKey;
+    private bool isAttaking = false;
     protected override void EnterAttackRange()
     {
+        if (!isAttaking)
+        {
+            StartCoroutine(Block(2f));
+        }
+    }
+
+    private IEnumerator Block(float time)
+    {
+        isAttaking = true;
         asc.TryActivateAbility(abilityKey);
+        yield return new WaitForSeconds(time);
+        isAttaking = false;
     }
 }
