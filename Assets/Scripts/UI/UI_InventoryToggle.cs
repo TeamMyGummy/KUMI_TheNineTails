a276c36c3e76ba427ff3879969248dd8798cb424
ohjinyxung <esujie339@ewhain.net>
2025-08-18 17:06:10 +0900

    Fix: HP바 깨짐, 인벤토리 클릭 안되는 오류 수정

--- Assets/Scripts/UI/UI_InventoryToggle.cs
+++ Assets/Scripts/UI/UI_InventoryToggle.cs
@@ -10,29 +10,16 @@ public class UI_InventoryToggle : MonoBehaviour
 
     private GameObject _panelInstance;
 
-    private void Awake()
-    {
-    
-    }
+    private void Awake() { }
 
-    public void OnClickInventoryIcon() => Toggle();
+    public void OnClickInventoryIcon()
+    {
+        Toggle();
+    }
 
     public void Toggle()
     {
-        if (_panelInstance == null)
-        {
-            _panelInstance = Instantiate(inventoryPanelPrefab, uiParent == null ? transform.root : uiParent);
-            _panelInstance.SetActive(true);
-
-            return;
-        }
-
-        _panelInstance.SetActive(!_panelInstance.activeSelf);
-    }
-
-
-    private void Close()
-    {
-        if (_panelInstance != null) _panelInstance.SetActive(false);
+        _panelInstance = Instantiate(inventoryPanelPrefab, uiParent == null ? transform.root : uiParent);
+        _panelInstance.SetActive(true);
     }
 }
