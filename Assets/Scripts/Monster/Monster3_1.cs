cd961aef9ba86917230766acb32497bd5c22fa62
daeun,Kang <kangda0519@gmail.com>
2025-08-19 18:46:29 +0900

    Feat: Monster3_1 구현

---
+++ Assets/Scripts/Monster/Monster3_1.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster3_1 : Monster
+{
+    public AbilityKey abilityKey;
+    public AbilityKey abilityKey2;
+    
+    private bool isAttaking = false;
+    
+    protected override void EnterShortAttackRange()
+    {
+        asc.TryActivateAbility(abilityKey);
+    }
+
+    protected override void EnterLongAttackRange()
+    {
+        if (!isAttaking)
+        {
+            StartCoroutine(Block(2f));
+        }
+    }
+
+    private IEnumerator Block(float time)
+    {
+        isAttaking = true;
+        asc.TryActivateAbility(abilityKey2);
+        yield return new WaitForSeconds(time);
+        isAttaking = false;
+    }
+}
