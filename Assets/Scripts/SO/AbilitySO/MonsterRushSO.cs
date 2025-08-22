1e51cc40acc8c2f54feac12003851b0290a41591
Gammja17 <haker_40@naver.com>
2025-08-22 19:12:53 +0900

    Fix: 몹 공격 관련 offset 변경
    rush도 수정

--- Assets/Scripts/SO/AbilitySO/MonsterRushSO.cs
+++ Assets/Scripts/SO/AbilitySO/MonsterRushSO.cs
@@ -8,7 +8,7 @@ public class MonsterRushSO : BlockAbilitySO
     [Header("공격 관련")]
     public float AttackRangeX = 1f;
     public float AttackRangeY = 1f;
-    public AttackDirection AttackDir = AttackDirection.Front;
+    public Vector2 RushOffset;
     public float RushDistance = 5f;
     public GameObject AttackHitboxPrefab;
     
