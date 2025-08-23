b97a34674ab1feecfc198f64537101e3de56c49b
ohjinyxung <esujie339@ewhain.net>
2025-08-23 21:16:03 +0900

    Design: 한글 폰트 적용

--- Assets/Scripts/UI/Popup/InputManager.cs
+++ Assets/Scripts/UI/Popup/InputManager.cs
@@ -32,11 +32,11 @@ public class InputManager : MonoBehaviour
         }
         else if (hasTabOpenedUI && Keyboard.current.qKey.wasPressedThisFrame)
         {
-            uiSkillInstance.OnClickSkillBtn();
+            uiSkillInstance.CycleRight();
         }
-        else if (hasTabOpenedUI && Keyboard.current.eKey.wasPressedThisFrame)
+        else if (hasTabOpenedUI && Keyboard.current.wKey.wasPressedThisFrame)
         {
-            uiSkillInstance.OnClickJournalBtn();
+            uiSkillInstance.CycleLeft();
         }
     }
 
