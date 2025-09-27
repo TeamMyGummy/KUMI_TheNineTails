a32d42d6fc0018b3dcaf67621d3a516a08d04ad1
SeungYeon Kang <syeon.dev@gmail.com>
2025-09-28 01:00:48 +0900

    Feat: 카메라 구현

---
+++ Assets/Scripts/Camera/CameraManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public enum ECameraState
+{
+    Targeting = 0, //default
+    StopAtPosition,
+}
+
+//기능이 많아진다 싶으면 아예 FSM으로 관리 아직은 현상유지(0923)
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField] private Vector3 targetOffset;
+    private const float cameraZ = -10f;
+    
+    //상태관리
+    private Transform target;
+    private ECameraState _state = ECameraState.Targeting;
+    private Vector2 _point;
+    
+    void Awake()
+    {
+        target = GameObject.FindWithTag("Player").transform;
+        transform.position = GetDesiredPosition();
+    }
+    void LateUpdate()
+    {
+        if (target == null) return;
+        Vector3 desiredPos = GetDesiredPosition();
+        Vector3 targetPoint = Vector3.MoveTowards(transform.position, desiredPos, 3f);
+        transform.position = new Vector3(targetPoint.x, targetPoint.y, cameraZ);
+    }
+
+    public Vector3 GetDesiredPosition()
+    {
+        return _state switch
+        {
+            ECameraState.Targeting => target.position + targetOffset,
+            ECameraState.StopAtPosition => _point,
+            _ => transform.position
+        };
+    }
+
+    public void StopCameraAtPoint(Vector2 pos)
+    {
+        _point = pos;
+        _state = ECameraState.StopAtPosition;
+    }
+
+    public void FollowTarget()
+    {
+        _state = ECameraState.Targeting;
+    }
+}
