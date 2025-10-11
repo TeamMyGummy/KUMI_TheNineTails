77e098ae56a8598c8b843fc1905c88e89cc89a22
SeungYeon Kang <syeon.dev@gmail.com>
2025-10-11 22:38:34 +0900

    Feat: 도깨비 먼지파도 위치 랜덤하게 스폰

---
+++ Assets/Scripts/BehaviorTree/Nodes/Pure/GetRandom.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using XNode;
+
+namespace BehaviorTree.Pure 
+{
+    public class GetRandom : Node //순수 노드 - 값을 연산하여 반환만 함
+    {
+        [SerializeField] public float startRange;
+        [SerializeField] public float endRange;
+        [Output(backingValue = ShowBackingValue.Never, connectionType = ConnectionType.Override)] 
+        public float value;
+        
+        public override object GetValue(XNode.NodePort port)
+        {
+            if (port.fieldName == "value") 
+                return Random.Range(startRange, endRange);
+            return null;
+        }
+    }
+}
