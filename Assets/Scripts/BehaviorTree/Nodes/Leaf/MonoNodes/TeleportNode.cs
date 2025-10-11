e0196188a77a8474f994d11d549cc7198ced74b9
SeungYeon Kang <syeon.dev@gmail.com>
2025-10-11 22:38:12 +0900

    Chore: BT 노드들 폴더 변경

--- Assets/Scripts/BehaviorTree/Nodes/Leaf/MonoNodes/TeleportNode.cs
+++ Assets/Scripts/BehaviorTree/Nodes/Leaf/MonoNodes/TeleportNode.cs
@@ -4,13 +4,18 @@ namespace BehaviorTree.Leaf
 {
     public class TeleportNode : MonoNode
     {
-        [SerializeField] public float x;
+        //[강승연] x, y의 Input을 받아서 조정하는 무언가가 또 생기면 전체 리팩토링
+        [SerializeField][Input] public float x;
         [SerializeField] public EPositionType xType;
         [SerializeField] public float y;
         [SerializeField] public EPositionType yType;
         
         protected override void OnEnter()
         {
+            if (GetPort("x").IsConnected)
+            {
+                x = GetInputValue<float>("x");
+            }
             if (runtimeHandler is TeleportHandler handler) 
                 handler.SetMovementPoint(new Vector3(x, y, 0f), xType, yType);
         }
