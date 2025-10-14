a986cdef54a06203c47fbc0eb99ff5aa813da18d
wwonnn <dnjsgml0508@naver.com>
2025-10-15 00:10:56 +0900

    Fix: Player Attack 충돌 안 되는 문제

--- Assets/Scripts/AbilitySystem/Abilities/PlayerAbility/PlayerAttack.cs
+++ Assets/Scripts/AbilitySystem/Abilities/PlayerAbility/PlayerAttack.cs
@@ -8,6 +8,7 @@ public class PlayerAttack : BlockAbility<BlockAbilitySO>
 {
     private PlayerAttackSO _so;
     private GameObject _hitbox;
+    private Collider2D _hitboxCollider;
     
     private readonly Vector2 _spawnPoint = new (0.55f, 0.98f);
     
@@ -18,7 +19,8 @@ public class PlayerAttack : BlockAbility<BlockAbilitySO>
         
         // Hitbox 초기화
         _hitbox = ResourcesManager.Instance.Instantiate(_so.Hitbox, actor.transform);
-        _hitbox.SetActive(false);
+        _hitboxCollider = _hitbox.GetComponent<Collider2D>();
+        _hitboxCollider.enabled = false;
     }
     
 
@@ -38,14 +40,18 @@ public class PlayerAttack : BlockAbility<BlockAbilitySO>
     
     private async UniTask SpawnHitbox()
     {
-        _hitbox.SetActive(true);
- 
         _hitbox.transform.localPosition = Actor.GetComponent<SpriteRenderer>().flipX 
             ? new Vector2(_spawnPoint.x * (-2), _spawnPoint.y) 
             : new Vector2(_spawnPoint.x, _spawnPoint.y);
         
+        // Collider만 비활성화했다가 활성화
+        _hitboxCollider.enabled = true;
+    
+        await UniTask.WaitForFixedUpdate(); // 물리 프레임에 등록될 때까지 대기
+        await UniTask.WaitForFixedUpdate(); 
+        
         await UniTask.Delay(TimeSpan.FromSeconds(0.1f));
         
-        _hitbox.SetActive(false);
+        _hitboxCollider.enabled = false;
     }
 }
