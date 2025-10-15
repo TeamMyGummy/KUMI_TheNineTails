94573b10322a9b57e5da8dac6230be0dfc210219
daeun,Kang <kangda0519@gmail.com>
2025-10-15 15:41:59 +0900

    Feat: tutorial 6번째 부분까지 구현

--- Assets/Scripts/Map/ConditionalDoor/ConditionDialogue.cs
+++ Assets/Scripts/Map/ConditionalDoor/ConditionDialogue.cs
@@ -6,11 +6,15 @@ public class ConditionDialogue : MonoBehaviour
 {
     [SerializeField] private string dialogueName;
     [SerializeField] private GameObject popup;
+    [SerializeField] private GameObject prevPopup;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            if (prevPopup != null)
+                prevPopup.SetActive(false);
+            
             YarnManager.Instance.RunDialogue(dialogueName, EnablePopup);
             
             gameObject.SetActive(false);
