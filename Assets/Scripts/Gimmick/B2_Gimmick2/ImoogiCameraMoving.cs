ee4689da1c9669eb4519c1a63f5ecc11ab5925ff
daeun,Kang <kangda0519@gmail.com>
2025-10-25 16:25:35 +0900

    Feat: gimmick2 시작 끝 카메라 연출 추가

--- Assets/Scripts/Gimmick/B2_Gimmick2/ImoogiCameraMoving.cs
+++ Assets/Scripts/Gimmick/B2_Gimmick2/ImoogiCameraMoving.cs
@@ -9,12 +9,24 @@ public class ImoogiCameraMoving : MonoBehaviour
     public void StartMoving()
     {
         CameraManager.Instance.StopCameraAtPoint(position);
+        StartCoroutine(Shaking());
         StartCoroutine(FollowPlayer());
+    }
+
+    public void ShakingCamera()
+    {
+        CameraManager.Instance.Shake(0.7f, 0.2f);
     }
 
     private IEnumerator FollowPlayer()
     {
         yield return new WaitForSeconds(3f);
         CameraManager.Instance.FollowTarget();
+    }
+
+    private IEnumerator Shaking()
+    {
+        yield return new WaitForSeconds(2f);
+        CameraManager.Instance.Shake(1f, 1.5f);
     }
 }
