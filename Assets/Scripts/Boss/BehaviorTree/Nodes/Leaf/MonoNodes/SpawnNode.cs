30ce47580581b479cd6a2ed979b974514b330a3d
SeungYeon Kang <syeon.dev@gmail.com>
2025-10-30 23:17:00 +0900

    Fix: 이무기 잡다한 오류 일괄 수정

--- Assets/Scripts/Boss/BehaviorTree/Nodes/Leaf/MonoNodes/SpawnNode.cs
+++ Assets/Scripts/Boss/BehaviorTree/Nodes/Leaf/MonoNodes/SpawnNode.cs
@@ -6,7 +6,9 @@ namespace BehaviorTree.Leaf
     public class SpawnNode : MonoNode
     {
         [Input] public Vector3 spawnPosition;
+        [Output(backingValue = ShowBackingValue.Never, connectionType = ConnectionType.Override)] public Vector3 location;
         [SerializeField] public float delay;
+        [SerializeField] private GameObject spawnObject;
         
         [SerializeField] public float x;
         [SerializeField] public EPositionType xType;
@@ -15,10 +17,17 @@ namespace BehaviorTree.Leaf
 
         protected override void OnEnter()
         {
-            Vector3 pos = GetInputValue<Vector3>("spawnPosition", spawnPosition);
+            location = GetInputValue<Vector3>("spawnPosition", spawnPosition);
             
             if (runtimeHandler is SpawnHandler handler) 
-                handler.SetPosition(pos, new Vector2(x, y), xType, yType, delay);
+                handler.SetPosition(location, new Vector2(x, y), xType, yType, delay, spawnObject);
+        }
+        
+        public override object GetValue(XNode.NodePort port)
+        {
+            if (port.fieldName == "location") 
+                return location;
+            return null;
         }
     }
 }
