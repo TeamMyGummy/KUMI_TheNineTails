3b2a4515c09624ffa0dfd0b2323fea7fdead976a
daeun,Kang <kangda0519@gmail.com>
2025-11-11 03:58:07 +0900

    Fix: 튜토리얼 자잘한거 수정

--- Assets/Scripts/Gimmick/Tutorial/HoldButton.cs
+++ Assets/Scripts/Gimmick/Tutorial/HoldButton.cs
@@ -9,28 +9,8 @@ public class HoldButton : MonoBehaviour
 {
     [SerializeField] private ConditionKey conditionKey;
     
-    [SerializeField] private Image progressBar;
-    [SerializeField] private float holdDuration = 3f; // 총 눌러야 하는 시간
     
-    private float _currentHoldTime = 0f;
-    private bool _isHolding = false;
-
-    private void Update()
-    {
-        if (_isHolding)
-        {
-            return;
-        }
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
-        {
-            _currentHoldTime += Time.deltaTime;
-            progressBar.fillAmount = _currentHoldTime / holdDuration;
-            if (_currentHoldTime >= holdDuration)
-            {
-                ConditionEventBus.Raise(conditionKey);
-                _isHolding = true;
-            }
-        }
-        
-    }
+    public void OpenDoor(){
+		ConditionEventBus.Raise(conditionKey);
+	}
 }
