490a125bdc4f018f0528fcf3ca2a7c7aebb4e045
gamuza <haker_40@naver.com>
2026-01-02 19:45:50 +0900

    Fix: 시작화면 리소스 적용 및 로직 변경

--- Assets/Scripts/UI/Popup/UI_Settings.cs
+++ Assets/Scripts/UI/Popup/UI_Settings.cs
@@ -4,14 +4,27 @@ using UnityEngine.UI;
 public class UI_SettingsPopup : MonoBehaviour
 {
     [SerializeField] private Button closeBtn;
+    private UI_StartMenu startMenu;
 
     private void Start()
     {
-        closeBtn.onClick.AddListener(ClosePopup);
+        startMenu = Object.FindFirstObjectByType<UI_StartMenu>();
+
+        if (closeBtn != null)
+        {
+            // 이 버튼은 인스펙터에서 Raycast Target이 켜져 있어야 합니다.
+            closeBtn.onClick.AddListener(ClosePopup);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) ClosePopup();
     }
 
     public void ClosePopup()
     {
+        if (startMenu != null) startMenu.CloseSettings();
         Destroy(gameObject);
     }
 }
